8b25ef8e3bee3cb7a131ade40e2b779f071bece1
Caroline Harrod <carolinejoharrod@gmail.com>
2024-08-14 22:26:57 -0700

    first commit of profile and settings screens

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,15 +1,25 @@
-import { Text, View } from "react-native";
+import { Text, View, Node } from "react-native";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
 
 export default function Profile() {
-    return (
+  return (
+    <div>
+      <h2 className="profile-head">Profile</h2>
       <View
         style={{
           flex: 1,
           justifyContent: "center",
           alignItems: "center",
-        }}
-      >
-        <Text>Edit app/profile.tsx to edit this screen.</Text>
+        }}>
+        <img src="../app/profile.png" className="profile-icon" />
+        <main className="profile">
+          <h3>Name</h3>
+          <h3>Email</h3>
+          <h3>Username</h3>
+        </main>
       </View>
-    );
-  }
+    </div>
+  );
+}
