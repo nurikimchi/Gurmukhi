63d2637c4a335b89e3dfca1d3ae838e820bb32c0
kirti <kirsharma09@gmail.com>
2024-08-15 21:58:59 -0700

    updated homescreen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,32 @@
-import { Text, View } from "react-native";
+import React from "react";
+import Navbar from "../Navbar";
+import Button from "../Button";
+import AnimatedProgress from "../AnimatedProgress"
+import Cards from "../Cards";
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+import "./style.css";
+import { Text, View, StyleSheet } from "react-native";
+
+
+export default function Index(){
+  const handleContinue = () => {
+    console.log('Continue button pressed');
+  }
+
+  return(
+    <View>
+      <Navbar />
+      <AnimatedProgress />
+      <Cards />
+      
+        <View 
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        >
+        </View>
     </View>
-  );
+  )
 }
