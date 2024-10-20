4096fcb120eab52c670008c0bf8c812a3530da91
Caramel4764 <nina.dong4764@gmail.com>
2024-10-20 13:59:24 -0700

    fix: disabling/enabling audio now saves outside a lesson

--- app/feature.jsx
+++ app/feature.jsx
@@ -148,7 +148,6 @@ import Design_ੜ_35 from "../constants/data/Design_ੜ_35.tsx";
 
 ]
 */
-
 import * as Progress from "react-native-progress";
 
 import "react-native-gesture-handler";
@@ -365,7 +364,16 @@ export default function Feature() {
 
 	const [globalCounter, setGlobalCounter] = React.useState(0);
 
-	const [isAudioEnabled, setIsAudioEnabled] = React.useState(true);
+	console.log('testing123')
+	console.log(JSON.parse(localStorage.getItem('isAudioEnabled')))
+	if (typeof JSON.parse(localStorage.getItem('isAudioEnabled')) === 'boolean') {
+		console.log('good')
+	} else {
+		localStorage.setItem("isAudioEnabled", true);
+		console.log('bad')
+	}
+	const [isAudioEnabled, setIsAudioEnabled] = React.useState(JSON.parse(localStorage.getItem('isAudioEnabled')));
+
 	const [status, setStatus] = React.useState("");
 
     const [ableToDraw, setAbleToDraw] = React.useState(true)
@@ -595,7 +603,10 @@ export default function Feature() {
 				</Pressable>
 			</View>
 
-			<View style={GlobalStyles.audioIconDiv} onClick={()=>setIsAudioEnabled(!isAudioEnabled)}>
+			<View style={GlobalStyles.audioIconDiv} onClick={()=>{
+				setIsAudioEnabled(!isAudioEnabled)
+				localStorage.setItem("isAudioEnabled", !isAudioEnabled);
+			}}>
 				{isAudioEnabled?<svg style={GlobalStyles.audioIcon} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="black">
 						<path stroke-linecap="round" stroke-linejoin="round" d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
 					</svg>:
